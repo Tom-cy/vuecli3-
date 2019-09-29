fa5940a2fb193676f2bb490570f249276b7ad5be
Tom-cy <18507040452@163.com>
2019-09-29 09:10:45 +0800

    entry

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,99 @@
 <template>
   <div>
-    login
+    <div>
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="demo-ruleForm">
+        <el-form-item label="用户名" prop="username" :label-width="labelWidth">
+          <el-input v-model="ruleForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="pass" :label-width="labelWidth">
+          <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="checkPass" :label-width="labelWidth">
+          <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item :label-width="labelWidth">
+          <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
+
+ 
+<script>
+export default {
+  data() {
+    var checkusername = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('年龄不能为空'))
+      }
+      setTimeout(() => {
+        // if (!Number.isInteger(value)) {
+        //   callback(new Error('请输入数字值'))
+        // } else {
+        //   if (value < 18) {
+        //     callback(new Error('必须年满18岁'))
+        //   } else {
+        //     callback()
+        //   }
+        // }
+      }, 1000)
+    }
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        if (this.ruleForm.checkPass !== '') {
+          this.$refs.ruleForm.validateField('checkPass')
+        }
+        callback()
+      }
+    }
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      labelWidth: '100px',
+      ruleForm: {
+        pass: '',
+        checkPass: '',
+        username: ''
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: 'blur' }],
+        checkPass: [{ validator: validatePass2, trigger: 'blur' }],
+        username: [{ validator: checkusername, trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
+    // resetForm(formName) {
+    //   this.$refs[formName].resetFields()
+    // }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.demo-ruleForm {
+  width: 350px;
+}
+</style>
